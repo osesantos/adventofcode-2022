956d437033ef71e638116cc07280117cf8de3c45
Jose Santos <jfnfsantos@gmail.com>
2023-05-26 23:11:28 +0100

    Day 2 part 1

--- scala/src/main/scala/day1/day2.scala
+++ scala/src/main/scala/day1/day2.scala
@@ -5,44 +5,47 @@ object day2 {
     getTotalScore(utils.ioutils.readFileLines(fileName)).toString
   }
 
-  /*
-  Opponent
-  A - 1
-  B - 2
-  c - 3
-
-  Me
-  X - 1
-  Y - 2
-  z - 3
-
-  Lost - 0
-  Draw - 3
-  Won  - 6
-  */
   private def getTotalScore(lines: List[String]): Int = lines.parseToScores.parseToFinalScore.sum
-
-  private def getMatchScore(a: Int, b: Int): Int = {
-    if a > b then return b + 0
-    if a == b then return b + 3
-    else return b + 6
-  }
 
   extension (lines: List[String])
     private def parseToScores: List[(Int, Int)] = lines.map(l => (l.substring(0, 1).parseToScore, l.substring(2, 3).parseToScore))
 
-  extension (input: String)
+  extension (input: String) {
     private def parseToScore: Int = input match
-      case "A" => 1
-      case "B" => 2
-      case "C" => 3
-      case "X" => 1
-      case "Y" => 2
-      case "Z" => 3
+      case "A" => 1 // Rock
+      case "B" => 2 // Paper
+      case "C" => 3 // Scissors
+      case "X" => 1 // Rock
+      case "Y" => 2 // Paper
+      case "Z" => 3 // Scissors
+  }
 
-  extension (scores: List[(Int, Int)])
+  extension (scores: List[(Int, Int)]) {
     private def parseToFinalScore: List[Int] = scores.map((a, b) => getMatchScore(a, b))
+  }
+
+  private def getMatchScore(a: Int, b: Int): Int = {
+    // Lost
+    if (a == 1 && b == 3) || (a == 2 && b == 1) || (a == 3 && b == 2) then {
+      return b
+    }
+    // Draw
+    if (a == 1 && b == 1) || (a == 2 && b == 2) || (a == 3 && b == 3) then {
+      return b + 3
+    }
+    // Win
+    else {
+      return b + 6
+    }
+  }
 
 
-
+  /**
+   * Y -> Draw = 3
+   * X -> Lose = 0
+   * Z -> Win  = 6
+   **/
+  def part2(fileName: String): String = {
+    getTotalScore(utils.ioutils.readFileLines(fileName)).toString
+  }
 }

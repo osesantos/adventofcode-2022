f7d68286e50fa752dc7d109d7eb32b261732bbf6
Jose Santos <jfnfsantos@gmail.com>
2023-05-27 00:51:21 +0100

    Bootstrap day 5

---
+++ rust/src/day5/mod.rs
@@ -0,0 +1,9 @@
+use crate::utils::get_file_lines;
+
+pub fn day5_1(file_name: &str) -> String {
+    "".to_string()
+}
+
+pub fn day5_2(file_name: &str) -> String {
+    "".to_string()
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,8 +1,9 @@
+use traits::PrintableResult;
 use crate::day1::{day1_1, day1_2};
 use crate::day2::{day2_1, day2_2};
-use traits::PrintableResult;
 use crate::day3::{day3_1, day3_2};
 use crate::day4::{day4_1, day4_2};
+use crate::day5::{day5_1, day5_2};
 
 mod utils;
 mod day1;
@@ -10,8 +11,15 @@ mod traits;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
+    println!();
+    println!("###############################################################");
+    println!(" Advent of Code 2022");
+
+    println!();
+
     day1_1("sample_day1.txt").printable_result("sample day 1.1:");
     day1_2("sample_day1.txt").printable_result("sample day 1.2:");
     day1_1("input_day1.txt").printable_result("day 1.1:");
@@ -37,6 +45,13 @@ fn main() {
     day4_2("sample_day4.txt").printable_result("sample day 4.2:");
     day4_1("input_day4.txt").printable_result("day 4.1:");
     day4_2("input_day4.txt").printable_result("day 4.2:");
+
+    println!();
+
+    day5_1("sample_day5.txt").printable_result("sample day 5.1:");
+    day5_2("sample_day5.txt").printable_result("sample day 5.2:");
+    day5_1("input_day5.txt").printable_result("day 5.1:");
+    day5_2("input_day5.txt").printable_result("day 5.2:");
 
     println!();
 }

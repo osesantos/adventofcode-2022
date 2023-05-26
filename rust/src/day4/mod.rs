c6fb3393324216ca6c114e3b3a2e4c9e1f5efdf6
Jose Santos <jfnfsantos@gmail.com>
2023-05-26 21:35:36 +0100

    adding day 4 rust

--- rust/src/day4/mod.rs
+++ rust/src/day4/mod.rs
@@ -1,7 +1,19 @@
 use crate::utils::get_file_lines;
 
 pub fn day4_1(file_name: &str) -> String {
+    let lines = get_file_lines(file_name);
+    let ranges: Vec<Vec<String>> = lines.iter().map(| l | l.split(",")).collect();
     "".to_string()
+}
+
+fn fully_contains(range1: &Vec<i64>, range2: &Vec<i64>) -> bool {
+    if range1.first() >= range2.first() && range1.last() <= range2.last() {
+        return true;
+    }
+    if range2.first() >= range1.first() && range2.last() <= range1.last() {
+        return true;
+    }
+    false
 }
 
 pub fn day4_2(file_name: &str) -> String {

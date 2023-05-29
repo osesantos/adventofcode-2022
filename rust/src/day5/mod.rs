f89c3c8dd7a402499ba358bfe454cb041cd8442a
Jose Santos <jfnfsantos@gmail.com>
2023-05-30 00:12:33 +0100

    Bootstrap day 5

--- rust/src/day5/mod.rs
+++ rust/src/day5/mod.rs
@@ -1,7 +1,37 @@
 use crate::utils::get_file_lines;
 
+struct Movement {
+    quantity: i32,
+    start_stack: i32,
+    end_stack: i32,
+}
+
 pub fn day5_1(file_name: &str) -> String {
+    let lines = get_file_lines(file_name);
+    let movements: Vec<_> = lines.iter().map(|l| l.movement_parser().unwrap_or(Movement{
+        quantity: 0,
+        start_stack: 0,
+        end_stack: 0,
+    })).into_iter().filter(|m| m.quantity != 0).collect();
+
     "".to_string()
+}
+
+trait MovementExtensions {
+    fn movement_parser(&self) -> Option<Movement>;
+}
+impl MovementExtensions for &String {
+    fn movement_parser(&self) -> Option<Movement> {
+        if !self.starts_with("move") {
+            return None;
+        }
+        let raw: Vec<_>= self.split(" ").collect();
+        return Some(Movement {
+            quantity: raw[1].parse::<i32>().unwrap(),
+            start_stack: raw[3].parse::<i32>().unwrap(),
+            end_stack: raw[5].parse::<i32>().unwrap(),
+        });
+    }
 }
 
 pub fn day5_2(file_name: &str) -> String {

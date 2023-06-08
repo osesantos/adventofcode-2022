6b00eaa82053ba10732d1873301f5f22d1027b46
Jose Santos <jfnfsantos@gmail.com>
2023-06-08 22:14:11 +0100

    day 1 - go

--- go/day1/day1_part2.go
+++ go/day1/day1_part2.go
@@ -2,6 +2,7 @@ package day1
 
 import (
 	"main/utils"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -15,24 +16,28 @@ func Part2(filename string) string {
 	var topCals []int
 	calCount := 0
 	lines.ForEach(func(index int, value string) {
-		if strings.Trim(value, " ") == "" {
-			if topCal < calCount {
-				topCal = calCount
-			}
-			calCount = 0
-			return
-		}
 		if i, errConv := strconv.Atoi(value); errConv == nil {
 			calCount = calCount + i
 		}
+
+		if strings.Trim(value, " ") == "" || index+1 == len(lines) {
+			topCals = append(topCals, calCount)
+			calCount = 0
+			return
+		}
 	})
 
-	return ""
+	sort.Slice(topCals, func(i, j int) bool {
+		return topCals[i] > topCals[j]
+	})
+
+	return strconv.Itoa(Sum(topCals[:3]))
 }
 
-// ContainLower Returns element index or error if does not find
-func ContainLower(i int, slice []int) (int, error) {
-	for _, value range slice {
-
+func Sum(slice []int) int {
+	sum := 0
+	for _, v := range slice {
+		sum += v
 	}
+	return sum
 }

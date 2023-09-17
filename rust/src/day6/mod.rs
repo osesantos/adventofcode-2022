437343ce617bf2486ab645f5db44a930709c547d
Jose Santos <jfnfsantos@gmail.com>
2023-09-17 09:49:28 +0100

    day 6 part 2

--- rust/src/day6/mod.rs
+++ rust/src/day6/mod.rs
@@ -28,12 +28,35 @@ fn get_chars_nth(n: usize, chars: Chars) -> char {
     chars.clone().nth(n).unwrap()
 }
 
+fn get_chars_nth_back(n: usize, chars: Chars) -> char {
+    chars.clone().nth_back(n).unwrap()
+}
+
 fn are_they_different(first: char, second: char, third: char, forth: char) -> bool {
     let chars: Vec<char> = vec![first, second, third, forth];
     let unique_chars: HashSet<_> = chars.iter().cloned().collect();
     unique_chars.len() == chars.len()
 }
 
+fn are_chars_different(chars: Vec<char>) -> bool {
+    let unique_chars: HashSet<_> = chars.iter().cloned().collect();
+    unique_chars.len() == chars.len()
+}
+
 pub fn day6_2(file_name: &str) -> String {
-    "".to_string()
+    let binding = get_file_lines(file_name);
+    let line = binding.first().unwrap();
+    let chars = line.chars();
+    let mut index: usize = 0;
+    chars.clone().enumerate().for_each(|(i, c)| {
+        if i > 12 && index == 0 {
+            let char_vec: Vec<char> = chars.clone().collect();
+            let slice = char_vec[i-13..i].to_vec();
+            if are_chars_different(slice) {
+                index = i + 1
+            }
+        }
+    });
+
+    index.to_string()
 }

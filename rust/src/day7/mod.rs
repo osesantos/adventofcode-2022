0fb241f9b634ba8a9d0e658087dd9b9b11e7756a
Jose Santos <jfnfsantos@gmail.com>
2023-09-18 09:24:01 +0100

    day 7 part 1

--- rust/src/day7/mod.rs
+++ rust/src/day7/mod.rs
@@ -1,6 +1,15 @@
 use crate::utils::get_file_lines;
 
+struct TreeEntry {
+    name: str,
+    size: usize,
+    children: Vec<TreeEntry>,
+}
+
 pub fn day7_1(file_name: &str) -> String {
+    let lines = get_file_lines(file_name);
+
+
     "".to_string()
 }
 
